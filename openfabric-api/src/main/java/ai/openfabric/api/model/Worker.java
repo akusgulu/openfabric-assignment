b38575ed2b69beece3630cbd9a630729c531b996
akusgulu <akusgulu309@gmail.com>
2023-03-31 11:44:13 +0300

    Add more info about containers in Worker entity

--- openfabric-api/src/main/java/ai/openfabric/api/model/Worker.java
+++ openfabric-api/src/main/java/ai/openfabric/api/model/Worker.java
@@ -1,14 +1,17 @@
 package ai.openfabric.api.model;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 import java.io.Serializable;
 
 @Entity()
@@ -23,5 +26,15 @@ public class Worker extends Datable implements Serializable {
 
     public String name;
 
+    public String command;
+
+    public String image;
+
+    public String status;
+
+    public String ports;
+
+    @OneToOne(mappedBy = "worker", cascade = CascadeType.ALL)
+    private WorkerStats stats;
 
 }
